9b74cce2aeaa78470e984ed872f65935fe5a1554
Ananya Kunjal <akunjal@ucsc.edu>
2026-07-22 19:48:04 -0700

    feat: implement Weekly Goals Modal

--- src/app/main/home/weekly-goals/weekly-goals-modal/weekly-goals-modal.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals-modal/weekly-goals-modal.component.ts
@@ -3,6 +3,18 @@ import { WeeklyGoalsModalAnimations } from './weekly-goals-modal.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { NgFor } from '@angular/common';
+import { WeeklyGoal } from '../../../../core/store/weekly-goal/weekly-goal.model';
+import { QuarterlyGoalData, WeeklyGoalInForm } from '../../home.model';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogClose} from '@angular/material/dialog';
+import { FormArray, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { endOfWeek, startOfWeek } from '../../../../core/utils/time.utils';
+import { CdkDragDrop, moveItemInArray, CdkDropList, CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-weekly-goals-modal',
@@ -11,7 +23,7 @@ import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: WeeklyGoalsModalAnimations,
   standalone: true,
-  imports: [
+  imports: [ MatIconModule, MatInputModule, MatSelectModule, MatButtonModule, FormsModule, ReactiveFormsModule, NgFor, CdkDropList, CdkDrag, CdkDragHandle, MatDialogClose,
   ],
 })
 export class WeeklyGoalsModalComponent implements OnInit {
@@ -26,19 +38,146 @@ export class WeeklyGoalsModalComponent implements OnInit {
   /** Loading icon. */
   loading: WritableSignal<boolean> = signal(false);
 
+  weeklyGoalsForm = this.fb.group({
+	allGoals: this.fb.array([
+	  this.fb.group({
+		text: ['', Validators.required],
+		originalText: [''],
+		originalOrder: [1],
+		__weeklyGoalId: [''],
+		__quarterlyGoalId: [''],
+		_deleted: [false],
+		_new: [false],
+	  }),
+	]),
+  });
   // --------------- COMPUTED DATA -----------------------
+  endOfWeek = endOfWeek;
+  startOfWeek = startOfWeek;
+
+  get allGoals() {
+	return this.weeklyGoalsForm.get('allGoals') as FormArray;
+  }
+
+  get addedGoalsCount() {
+    return this.allGoals.controls.filter(
+      (goal) => goal.value._new && !goal.value._deleted
+    ).length;
+  }
+
+  get editedGoalsCount() {
+    return this.allGoals.controls.filter(
+      (goal) =>
+        goal.dirty &&
+        goal.value.text !== goal.value.originalText &&
+        !goal.value._new &&
+        !goal.value._deleted
+    ).length;
+  }
+
+  get deletedGoalsCount() {
+    return this.allGoals.controls.filter((goal) => goal.value._deleted).length;
+  }
 
   // --------------- EVENT HANDLING ----------------------
+  addGoalToForm(goal: WeeklyGoalInForm) {
+	if (goal) {
+	  this.allGoals.push(
+		this.fb.group({
+		  text: [goal.text, Validators.required],
+		  originalText: [goal.text],
+		  originalOrder: [goal.originalOrder],
+		  originalQuarterlyGoalId: [goal.__quarterlyGoalId],
+		  __weeklyGoalId: [goal.__weeklyGoalId],
+		  __quarterlyGoalId: [goal.__quarterlyGoalId, Validators.required],
+		  _deleted: [false],
+		  _new: [false],
+		}),
+	  );
+	} else {
+	  this.allGoals.push(
+		this.fb.group({
+		  text: ['', Validators.required],
+		  __quarterlyGoalId: ['', Validators.required],
+		  _deleted: [false],
+		  _new: [true],
+		}),
+	  );
+	}
+  }
 
+  drop(event: CdkDragDrop<WeeklyGoal[]>) {
+	moveItemInArray(
+	  this.allGoals.controls,
+	  event.previousIndex,
+	  event.currentIndex,
+	);
+  }
+
+  moveItemInFormArray(
+	formArray: FormArray,
+	fromIndex: number,
+	toIndex: number,
+  ) {
+	const dir = toIndex > fromIndex ? 1 : -1;
+
+	const from = fromIndex;
+	const to = toIndex;
+
+	const temp = formArray.at(from);
+	for (let i = from; i * dir < to * dir; i = i + dir) {
+	  const current = formArray.at(i + dir);
+	  formArray.setControl(i, current);
+	}
+	formArray.setControl(to, temp);
+  }
+
+  fullDelete(e, i) {
+	if (
+	  e.target.checked &&
+	  this.weeklyGoalsForm.get(['allGoals', i, '_new']).value
+	) {
+	  this.allGoals.removeAt(i);
+	}
+  }
+
+  async saveGoals() {
+	await this.data.updateWeeklyGoals(this.allGoals);
+  }
+  
   // --------------- OTHER -------------------------------
 
   constructor(
     private injector: Injector,
-    @Inject(BATCH_WRITE_SERVICE) private batch: BatchWriteService,
-  ) { }
+    @Inject(MAT_DIALOG_DATA)
+	public data: {
+	  goalDatas: Partial<QuarterlyGoalData>[];
+	  incompleteGoals: WeeklyGoal[];
+      updateWeeklyGoals: (weeklyGoalsFormArray: FormArray) => void;
+	},
+    public dialogRef: MatDialogRef<WeeklyGoalsModalComponent>,
+	private fb: FormBuilder,
+  ) {
+    this.allGoals.clear();
+    if (this.data.incompleteGoals.length == 0) {
+      this.addGoalToForm(null);
+    } else {
+      this.data.incompleteGoals.forEach((goal) => {
+        this.addGoalToForm({
+          text: goal.text,
+          __quarterlyGoalId: goal.__quarterlyGoalId,
+          originalText: goal.text,
+          originalOrder: goal.order,
+          originalQuarterlyGoalId: goal.__quarterlyGoalId,
+          __weeklyGoalId: goal.__id,
+          _deleted: goal._deleted,
+          _new: false,
+        });
+      });
+    }
+  }
 
   // --------------- LOAD AND CLEANUP --------------------
   
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 }
--- src/app/main/home/weekly-goals/weekly-goals.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals.component.ts
@@ -3,6 +3,12 @@ import { WeeklyGoalsAnimations } from './weekly-goals.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
+import { WeeklyGoalsHeaderComponent } from './weekly-goals-header/weekly-goals-header.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { QuarterlyGoalData, WeeklyGoalData } from '../home.model'; 
+import { Timestamp } from '@angular/fire/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-weekly-goals',
@@ -11,34 +17,174 @@ import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: WeeklyGoalsAnimations,
   standalone: true,
-  imports: [
+  imports: [WeeklyGoalsModalComponent, WeeklyGoalsHeaderComponent, 
   ],
 })
 export class WeeklyGoalsComponent implements OnInit {
   readonly authStore = inject(AuthStore);
   // --------------- INPUTS AND OUTPUTS ------------------
+  incompleteWeeklyGoals: Signal<WeeklyGoalData[]> = signal([
+    {
+      __id: 'wg1',
+      __userId: 'test-user',
+      __quarterlyGoalId: 'qg1',
+      __hashtagId: 'ht1',
+      text: 'Finish Google Cover Letter',
+      completed: false,
+      order: 1,
+      hashtag: {
+        __id: 'ht1',
+        __userId: 'test-user',
+        name: 'coverletter',
+        color: '#EE8B72',
+      },
+    },
+    {
+      __id: 'wg2',
+      __userId: 'test-user',
+      __quarterlyGoalId: 'qg2',
+      __hashtagId: 'ht2',
+      text: 'Apply to Microsoft',
+      completed: false,
+      order: 2,
+      hashtag: {
+        __id: 'ht2',
+        __userId: 'test-user',
+        name: 'apply',
+        color: '#2DBDB1',
+      },
+    },
+  ]);
 
+  completeWeeklyGoals: Signal<WeeklyGoalData[]> = signal([
+    {
+      __id: 'wg3',
+      __userId: 'test-user',
+      __quarterlyGoalId: 'qg3',
+      __hashtagId: 'ht3',
+      text: 'Review data structures',
+      completed: true,
+      order: 3,
+      hashtag: {
+        __id: 'ht3',
+        __userId: 'test-user',
+        name: 'interview',
+        color: '#FFB987',
+        _deleted: false,
+      },
+    },
+  ]);
+
+  quarterlyGoals: Signal<QuarterlyGoalData[]> = signal([
+    {
+      __id: 'qg1',
+      __userId: 'test-user',
+      __hashtagId: 'ht1',
+      text: 'Finish cover letters',
+      completed: false,
+      order: 1,
+      _createdAt: Timestamp.now(),
+      _updatedAt: Timestamp.now(),
+      _deleted: false,
+      hashtag: {
+        __id: 'ht1',
+        __userId: 'test-user',
+        name: 'coverletter',
+        color: '#EE8B72',
+        _createdAt: Timestamp.now(),
+        _updatedAt: Timestamp.now(),
+        _deleted: false,
+      },
+      weeklyGoalsTotal: 1,
+      weeklyGoalsComplete: 0,
+    },
+    {
+      __id: 'qg2',
+      __userId: 'test-user',
+      __hashtagId: 'ht2',
+      text: 'Apply to internships',
+      completed: false,
+      order: 2,
+      _createdAt: Timestamp.now(),
+      _updatedAt: Timestamp.now(),
+      _deleted: false,
+      hashtag: {
+        __id: 'ht2',
+        __userId: 'test-user',
+        name: 'apply',
+        color: '#2DBDB1',
+        _createdAt: Timestamp.now(),
+        _updatedAt: Timestamp.now(),
+        _deleted: false,
+      },
+      weeklyGoalsTotal: 1,
+      weeklyGoalsComplete: 0,
+    },
+    {
+      __id: 'qg3',
+      __userId: 'test-user',
+      __hashtagId: 'ht3',
+      text: 'Technical interview prep!',
+      completed: false,
+      order: 3,
+      _createdAt: Timestamp.now(),
+      _updatedAt: Timestamp.now(),
+      _deleted: false,
+      hashtag: {
+        __id: 'ht3',
+        __userId: 'test-user',
+        name: 'interview',
+        color: '#FFB987',
+        _createdAt: Timestamp.now(),
+        _updatedAt: Timestamp.now(),
+        _deleted: false,
+      },
+      weeklyGoalsTotal: 1,
+      weeklyGoalsComplete: 1,
+    },
+  ]);
   /** The current signed in user. */
   currentUser: Signal<User> = this.authStore.user;
 
   // --------------- LOCAL UI STATE ----------------------
-
+  dialogRef: MatDialogRef<any>;
   /** Loading icon. */
   loading: WritableSignal<boolean> = signal(false);
 
   // --------------- COMPUTED DATA -----------------------
 
   // --------------- EVENT HANDLING ----------------------
-
+  openModal(editClicked: boolean){
+    this.dialogRef = this.dialog.open(WeeklyGoalsModalComponent, {
+      height: '90%',
+      position: { bottom: '0' },
+      panelClass: 'goal-modal-panel',
+      data: {
+        goalDatas: this.quarterlyGoals(),
+        incompleteGoals: this.incompleteWeeklyGoals(),
+       updateWeeklyGoals: async (weeklyGoalsFormArray) => {
+          try {
+            this.snackBar.open('Goals were updated', '', {
+              duration: 3000,
+              verticalPosition: 'bottom',
+              horizontalPosition: 'center',
+            });
+            this.dialogRef.close();
+          } catch (e) {
+            console.error(e);
+          }
+        },
+      },
+    });
+  }
   // --------------- OTHER -------------------------------
 
   constructor(
-    private injector: Injector,
-    @Inject(BATCH_WRITE_SERVICE) private batch: BatchWriteService,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar,
   ) { }
 
   // --------------- LOAD AND CLEANUP --------------------
   
-  ngOnInit(): void {
-  }
+  ngOnInit() {}
 }
